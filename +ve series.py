94146ec2f0e462954a54eefc5efc491906967f22
Gayathri Gnanamoorthy <71868720+GayathriGnanamoorthy@users.noreply.github.com>
2023-06-10 14:21:52 +0530

    Update +ve series.py

--- +ve series.py
+++ +ve series.py
@@ -1,9 +1,15 @@
-start = int(input("Enter the start of range: "))
-end = int(input("Enter the end of range: "))
-  
-# iterating each number in list
-for num in range(start, end + 1):
-  
-    # checking condition
-    if num >= 0:
-        print(num, end=" ")
+def print_positive_numbers(numbers):
+    positive_numbers = [num for num in numbers if num > 0]
+    print(positive_numbers)
+
+# Example 1
+list1 = [12, -7, 5, 64, -14]
+print("Input:", list1)
+print("Output:", end=" ")
+print_positive_numbers(list1)
+
+# Example 2
+list2 = [12, 14, -95, 3]
+print("Input:", list2)
+print("Output:", end=" ")
+print_positive_numbers(list2)
